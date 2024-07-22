6e21aae0c58a9da1fa97bd2922fb58e1bc544049
Joshua Rauco <jrauco@pesco.local>
2024-07-22 11:26:43 -0400

    github

--- apps/camiones/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CamionesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.camiones'
--- apps/repuestos/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def lista_view(request):
-    return render(request, 'repuestos/lista.html')
